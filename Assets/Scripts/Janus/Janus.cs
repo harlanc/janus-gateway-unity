472725a698a8d53731891780b3fc452bac1e8e36
Harlan <hailiang@gvrcraft.com>
2018-07-12 07:38:10 +0800

    init janus-gateway-unity,only contains audio bridge plugin

---
+++ Assets/Scripts/Janus/Janus.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Application
+{
+    public class Janus
+    {
+        private static  AndroidJavaObject jObj_janusclientAPI = null; 
+        private static AndroidJavaObject jObj_curcontext = null; 
+
+        private static AndroidJavaObject GetJavaObject_janusclientAPI()
+        {
+            if (jObj_janusclientAPI == null)
+            {
+                jObj_janusclientAPI = new AndroidJavaObject("computician.janusclientapi.JanusAudioBridge");
+            }
+            return jObj_janusclientAPI;
+        }
+
+
+        public static AndroidJavaObject GetContext(){
+
+            if(jObj_curcontext == null){
+                AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+                jObj_curcontext = activity.Call<AndroidJavaObject>("getApplicationContext");
+                
+            }
+            return jObj_curcontext;
+        }
+
+        public   void setContext()
+        {
+            GetJavaObject_janusclientAPI().Call("SetContext", GetContext());
+            
+        }
+        public  void init()
+        {
+            GetJavaObject_janusclientAPI().Call("init");
+        }
+
+        public  void openMic()
+        {
+            GetJavaObject_janusclientAPI().Call("openMic");
+            
+        }
+        public void closeMic()
+        {
+            GetJavaObject_janusclientAPI().Call("closeMic");
+
+        }
+
+        public void closeSpeaker()
+        {
+            GetJavaObject_janusclientAPI().Call("closeSpeaker");
+
+        }
+
+        public void openSpeaker()
+        {
+            GetJavaObject_janusclientAPI().Call("openSpeaker");
+        }
+
+
+
+    }
+}
---
+++ Assets/VideoBridgeController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using Application;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class VideoBridgeController : MonoBehaviour
+{
+
+    public static bool micOpen = false;
+    public static bool speakerOpen = false;
+
+    Janus voicecontroller = new Janus();
+    // Use this for initialization
+    void Start()
+    {
+        voicecontroller.setContext();
+        voicecontroller.init();
+    
+
+        //voicecontroller.openMic();
+        //voicecontroller.openSpeaker();
+
+        List<string> btnsName = new List<string>();
+        btnsName.Add("BtnOpenMic");
+        btnsName.Add("BtnOpenSpeaker");
+   
+ 
+        foreach(string btnName in btnsName)
+        {
+            GameObject btnObj = GameObject.Find(btnName);
+            Button btn = btnObj.GetComponent<Button>();
+            btn.onClick.AddListener(delegate() {
+                this.OnClick(btnObj); 
+            });
+        } 
+
+    }
+
+    public void OnClick(GameObject sender)
+    {
+        switch (sender.name)
+        {
+            case "BtnOpenMic":
+                Debug.Log("BtnOpenMic");
+                if (micOpen)
+                {
+                    voicecontroller.closeMic();
+                }
+                else
+                {
+                    voicecontroller.openMic();
+                }
+                micOpen = !micOpen;
+                break;
+            case "BtnOpenSpeaker":
+                Debug.Log("BtnOpenSpeaker");
+                if (speakerOpen)
+                {
+                    voicecontroller.closeSpeaker();
+                }
+                else
+                {
+                    voicecontroller.openSpeaker();
+                }
+                speakerOpen = !speakerOpen;
+                break;
+              default:
+                Debug.Log("none");
+                break;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
